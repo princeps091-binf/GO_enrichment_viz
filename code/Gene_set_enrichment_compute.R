9d275a2dd844ac6f8cc0ef3ed2b67319840d253d
princeps091-binf <princeps091@gmail.com>
2022-03-08 16:58:35 +0100

    examined difference between compound and single res hubs

--- code/Gene_set_enrichment_compute.R
+++ code/Gene_set_enrichment_compute.R
@@ -1,18 +1,43 @@
 library(tidyverse)
+library(GenomicRanges)
 #------------------------------
 get_obj_in_fn<-function(file){
-  tmp_tbl<-get(load(file))
+  out_tbl<-get(load(file))
   tmp_obj<-names(mget(load(file)))
   rm(list=tmp_obj)
   rm(tmp_obj)
-  return(tmp_tbl)
+  return(out_tbl)
+}
+
+GO_set_enrich_fn<-function(cl_set_gene,cage_active_genes_vec,GOBP_set){
+  fn_env<-environment()
+  
+  cl<-makeCluster(5)
+  clusterEvalQ(cl, {
+    library(dplyr)
+    print('node ready')
+  })
+  clusterExport(cl,c('cl_set_gene','cage_active_genes_vec'),envir = fn_env)
+  go_pval<-parLapply(cl,GOBP_set,function(tmp_set){
+    hitInSample<-sum(cl_set_gene %in% tmp_set)
+    sampleSize<-length(cl_set_gene)
+    hitInPop<-sum(cage_active_genes_vec %in% tmp_set)
+    failInPop<-length(cage_active_genes_vec) - hitInPop
+    p_val<-phyper(hitInSample-1, hitInPop, failInPop, sampleSize, lower.tail= FALSE)
+    OR_GO<-(hitInSample/sampleSize)/(hitInPop/length(cage_active_genes_vec))
+    return(tibble(p.val=p_val,OR=OR_GO,in.gene=hitInSample))
+  })
+  stopCluster(cl)
+  rm(cl)
+  path_tbl<-do.call(bind_rows,go_pval)%>%mutate(Gene.Set=names(go_pval),FDR=p.adjust(p.val,method='fdr'))%>%dplyr::select(Gene.Set,FDR,OR,in.gene)
+  return(path_tbl)
 }
 
 #------------------------------
-background_gene_file<-"./data/CAGE_GM12878_gene_GRange.Rda"
-foreground_gene_file<-"./data/GM12878_50kb_hub_ENSG_tbl.Rda"
+background_gene_file<-"./data/CAGE_H1_gene_GRange.Rda"
+foreground_gene_file<-"./data/H1_5kb_tss_compound_hub_ENSG_tbl.Rda"
 
-out_file<-"./data/GM12878_hub_5kb_stub_CAGE_GOBP_enrich_tbl.Rda"
+#out_file<-"./data/hub_5kb_HMEC_CAGE_rich_GOBP_enrich_tbl.Rda"
 
 gene_conv_tbl_file<-"./data/gene_name_conv_tbl.Rda"
 
@@ -40,29 +65,6 @@ background_gene_vec<-gene_conv_tbl%>%
   distinct(entrezgene_id) %>% unlist %>% as.character
 
 #hg19_vec<-gene_conv_tbl %>% distinct(entrezgene_id) %>% unlist
-GO_set_enrich_fn<-function(cl_set_gene,cage_active_genes_vec,GOBP_set){
-  fn_env<-environment()
-  
-  cl<-makeCluster(5)
-  clusterEvalQ(cl, {
-    library(dplyr)
-    print('node ready')
-  })
-  clusterExport(cl,c('cl_set_gene','cage_active_genes_vec'),envir = fn_env)
-  go_pval<-parLapply(cl,GOBP_set,function(tmp_set){
-    hitInSample<-sum(cl_set_gene %in% tmp_set)
-    sampleSize<-length(cl_set_gene)
-    hitInPop<-sum(cage_active_genes_vec %in% tmp_set)
-    failInPop<-length(cage_active_genes_vec) - hitInPop
-    p_val<-phyper(hitInSample-1, hitInPop, failInPop, sampleSize, lower.tail= FALSE)
-    OR_GO<-(hitInSample/sampleSize)/(hitInPop/length(cage_active_genes_vec))
-    return(tibble(p.val=p_val,OR=OR_GO,in.gene=hitInSample))
-  })
-  stopCluster(cl)
-  rm(cl)
-  path_tbl<-do.call(bind_rows,go_pval)%>%mutate(Gene.Set=names(go_pval),FDR=p.adjust(p.val,method='fdr'))%>%dplyr::select(Gene.Set,FDR,OR,in.gene)
-  return(path_tbl)
-}
 
 
 path_tbl<-GO_set_enrich_fn(foreground_gene_vec,background_gene_vec,Gene_set_l)
--- code/compound_hub_gene_content_compute.R
+++ code/compound_hub_gene_content_compute.R
@@ -15,16 +15,16 @@ get_obj_in_fn<-function(file){
   return(out_tbl)
 }
 #-------------------------------------------------------------------
-cl_tbl_file<-"~/Documents/multires_bhicect/Bootstrapp_fn/data/candidate_compound_hub/GM12878_hub_5kb_stub.Rda"
-cl_spec_res_folder<-"~/Documents/multires_bhicect/data/GM12878/spec_res/"
+cl_tbl_file<-"~/Documents/multires_bhicect/Bootstrapp_fn/data/candidate_compound_hub/H1_non_5kb_stub_hub.Rda"
+cl_spec_res_folder<-"~/Documents/multires_bhicect/data/H1/Dekker/spec_res/"
 
-gene_GRange_file<-"./data/CAGE_GM12878_gene_GRange.Rda"
+gene_GRange_file<-"./data/CAGE_H1_gene_GRange.Rda"
 
-out_file<-paste0("./data/GM12878_hub_5kb_stub_ENSG_tbl.Rda")
+out_file<-paste0("./data/H1_non_5kb_stub_hub_ENSG_tbl.Rda")
 
 cl_tbl<-get_obj_in_fn(cl_tbl_file)
 cl_tbl<-cl_tbl %>% 
-  mutate(res=map_chr(hub.5kb,function(x){strsplit(x,split="_")[[1]][1]}))
+  mutate(res=map_chr(hub,function(x){strsplit(x,split="_")[[1]][1]}))
 gene_GRange<-get_obj_in_fn(gene_GRange_file)
 
 #Build BHiCect GRange
@@ -34,7 +34,7 @@ names(chr_res_l)<-chr_set
 for (chromo in chr_set){
   message(chromo)
   load(paste0(cl_spec_res_folder,chromo,"_spec_res.Rda"))
-  chr_cl_tbl<-cl_tbl %>% filter(chr==chromo) %>% mutate(bins=chr_spec_res$cl_member[hub.5kb])
+  chr_cl_tbl<-cl_tbl %>% filter(chr==chromo) %>% mutate(bins=chr_spec_res$cl_member[hub])
   plan(multisession, workers = 3)
   
   chr_cl_tbl<-chr_cl_tbl %>% mutate(GRange=future_pmap(list(chr,bins,res),function(chr,bins,res){
